b773e7735b95f123ad6a7ea6f186ed4dd2c30c9a
Georgestamos707 <gs68@juno.com>
2023-03-24 15:40:00 +0000

    add conditionals solution

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -6,24 +6,25 @@
 # Based on what the player chose, prints "You won!" or "You lost!" or "You tied!"
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
-def rock_paper_scissor
-
-end
-
 p "Please choose rock, paper, or scissors:"
-if paper < scissors
-p "You played paper!"
-P "The computer played scissors!"
-p "You lost!"
-elsif scissors == scissors
-  p "You played scissors!"
-  p "The computer played scissors"
-  p "Its a tie!"
-elsif rock > scissors
-  p "You played rock!"
-  p "The computer played scissors!"
+
+answer = gets.chomp
+
+# "You played rock!"
+p "You played #{answer}!"
+
+p "The computer played scissors!"
+
+# Based on what the player chose, prints "You won!" or "You lost!" or "You tied!"
+if answer == 'rock'
   p "You won!"
-end 
+elsif answer == 'paper'
+  p "You lost!" 
+elsif answer == "scissors"
+  p "You tied!"
+end
+ 
+
 
 
 
